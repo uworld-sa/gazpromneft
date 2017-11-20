6f5e302d344c01b90f76519d3425a9c1ee59c22e
uw <uw@colo113.tuthost.com>
2017-11-20 17:21:29 +0200

    site

---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/screen.css">
+	<meta name="viewport" content="width=device-width">
+	<script type="text/javascript" src="/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/jquery-asPieProgress.min.js"></script>
+	<script type="text/javascript" src="/js/waypoints.min.js"></script>
+	<script type="text/javascript" src="/js/script.js"></script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KX9ZXT');</script>
+	<!-- End Google Tag Manager -->
+	<meta property="og:title" content="title" />
+	<meta property="og:description" content="description" />
+	<meta property="og:url" content="http://gazpromneft.uw-t.com/" />
+	<meta property="og:image" content="http://ia.media-imdb.com/images/rock.jpg" />
+	<?php 
+		$url = 'http://ecomonitoring.tass.ru/'; 
+		$title = 'ДЫШИТЕ ГЛУБЖЕ'; 
+		$image = 'http://ecomonitoring.tass.ru/gpneft-share.jpg'; 
+		$desc = 'Как новые стандарты экомониторинга делают атмосферу Москвы прозрачнее'; 
+	?>
+	<link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
+</head>
+<body>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KX9ZXT"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
+
+	<div id="page-wrapper">
+		
+		<div id="all-wrapper">
+			
+			<div id="screen-1">
+				<img class="bg" src="images/header.svg"/>
+				<div class="container">
+					<div class="title">
+						Дышите глубже 
+					</div>
+					<div class="sub-title">
+						как новые стандарты экомониторинга делают<br/> атмосферу Москвы прозрачнее
+					</div>
+				</div>
+				
+				<div class="scroll-next"><a href="#screen-2"><img src="/images/scroll.png" alt=""></a></div>
+				 <div class="shares">
+		            <ul class="clearfix">
+		                <li><a href="javascript::void(0)" onclick="Share.facebook('<?=$url?>', '<?=$title?>', '<?=$image?>', '<?=$desc?>')" class="share-btn facebook"><img src="images/fb.svg" /></a></li>
+		                <li><a href="javascript::void(0)" onclick="Share.vkontakte('<?=$url?>', '<?=$title?>', '<?=$image?>', '<?=$desc?>')" class="share-btn vk"><img src="images/vk.svg" /></a></li>
+		                <li><a href="javascript::void(0)" onclick="Share.twitter('<?=$url?>', '<?=$title?>')" class="share-btn twitter"><img src="images/tw.svg" /></a></li>
+		            </ul>
+		        </div>
+			</div>
+
+			<div id="screen-2">
+				<div class="container">
+					<div class="title">
+						В Год экологии Министерство природы РФ готовит предложения<br/> для улучшения системы экологического контроля.
+					</div>
+					<div class="image"><img src="/images/donskoy.png" alt=""></div>
+					<blockquote>
+						«Мы готовим изменения в законодательство по поручению президента.<br/> Основной документ предполагает, что мы, модернизируя предприятия,<br/> переводим их на новые экологически чистые технологии»
+					</blockquote>
+					<div class="position">глава Минприроды РФ</div>
+					<div class="name">Сергей Донской</div>
+				</div>
+			</div>		
+			
+			<div id="screen-3">
+				<div class="container">
+					<div class="block block-1">
+						<div class="block__title">
+							Изменения позволят рассчитывать объем выбросов от всех<br/> промышленных, а также транспортных объектов.
+						</div>
+						<div class="block__content">
+							Примером комплексного контроля окружающей среды является Москва.<br/>
+							За экологической обстановкой в столице уже давно следят самые разные<br/> системы. Например, больше 20 лет функционирует московская система<br/> мониторинга атмосферного воздуха, которая считается одной из лучших<br/>
+							в мире. На сегодняшний день она состоит из 56 автоматических станций<br/> мониторинга загрязнения атмосферы, среди которых есть и мобильные.<br/>
+							Эти станции контролируют состояние воздуха в жилых зонах, вблизи автотрасс и предприятий. Системой управляет «Мосэкомониторинг».
+						</div>
+					</div>
+
+					<div class="title">КАК УСТРОЕНА СИСТЕМА ЭКОЛОГИЧЕСКОГО МОНИТОРИНГА МОСКВЫ</div>
+
+				</div>
+
+
+				<div class="auto-56">
+					<iframe width="100%" height="315" src="https://www.youtube.com/embed/XG2h_zGj87k?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+					<?php /*<img src="/images/56-auto.jpg" alt="">*/?>
+				</div>
+
+			</div>
+
+			<div id="screen-4">
+				<div class="container">
+					<div class="title">ЧТО КОНТРОЛИРУЕТ<br/> СИСТЕМА ЭКОЛОГИЧЕСКОГО МОНИТОРИНГА В МОСКВЕ</div>
+					<div class="what-control">
+						<div class="wc wc-1">
+							<div class="wc-image">
+								<div class="image"><img src="/images/wc__img-1.png" alt=""></div>
+								<div class="wc-title">12,5 млн.</div>
+								<div class="sub-title">человек</div>
+							</div>
+							<div class="wc-description">
+								По оценке экологической организации «Зеленый патруль»,<br/>
+								по совокупности факторов столица входит в десятку российских<br/> регионов по качеству экологической обстановки, но проблемы<br/> остаются. Население региона приближается к 12,5 млн человек.<br/>
+								Для их комфортной жизни необходима обширная инфраструктура. 
+							</div>
+						</div>
+						<div class="wc wc-2">
+							<div class="wc-image">
+								<div class="image"><img src="/images/wc__img-2.png" alt=""></div>
+								<div class="wc-title">8 млн.</div>
+								<div class="sub-title">автомобилей</div>
+							</div>
+							<div class="wc-description">
+								Тем временем автомобильный парк города и Московской области<br/> перешагнул отметку в 8 млн транспортных средств. Неудивительно,<br/> что в таких условиях, при постоянно растущем количестве<br/> автомобилей, именно они являются основным источником вредных<br/> выбросов. 
+							</div>
+						</div>
+						<div class="wc wc-3">
+							<div class="wc-image">
+								<div class="image"><img src="/images/wc__img-3.png" alt=""></div>
+								<div class="wc-title">98%</div>
+								<div class="sub-title">выбросов</div>
+							</div>
+							<div class="wc-description">
+								«В Москве практически не осталось промышленности, но есть<br/> автомобили, 98% вредных выхлопов в атмосферу в Москве -<br/>
+								это автомобильный транспорт», - говорил спецпредставитель<br/> президента РФ по вопросам экологии и природоохранной<br/> деятельности Сергей Иванов. 
+							</div>
+						</div>
+						<div class="wc wc-4">
+							<div class="wc-image">
+								<div class="image"><img src="/images/wc__img-4.png" alt=""></div>
+								<div class="wc-title">2%</div>
+								<div class="sub-title">выбросов</div>
+							</div>
+							<div class="wc-description">
+								Оставшиеся 2% выбросов производят предприятия, без которых<br/> городу не обойтись: ТЭЦ, очистные сооружения,<br/> нефтеперерабатывающий и мусоросжигательные заводы.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="screen-5">
+				<div class="container">
+					<div class="block block-2">
+						<div class="block__title">
+							РАЗВИТИЕ СИСТЕМЫ ЭКОЛОГИЧЕСКОГО МОНИТОРИНГА В МОСКВЕ
+						</div>
+						<div class="block__content">
+							<p>Внедрение датчиков контроля на промышленных, прежде всего, коммерческих<br/> предприятиях – следующий этап развития столичной системы мониторинга.<br/> Такие системы позволят отслеживать объем выбросов непосредственно на<br/> источниках загрязнения, в частности – на трубах.</p>
+							<p>С этих датчиков данные также будут поступать непосредственно в<br/> «Мосэкомониторинг». Такой подход дополняет работу постов, которые<br/> находятся за заборами предприятий, в жилых зонах, у дорог. Датчики на трубах позволят более отчетливо показывать ситуацию с выбросами.</p> 
+							<p>В коммерческом секторе такие датчики установлены только на Московском<br/> НПЗ компании «Газпром нефть». Внедряемая здесь с 2015 года<br/> автоматизированная система мониторинга атмосферного воздуха (АСМВ)<br/> стала пилотным проектом российской нефтеперерабатывающей отрасли в<br/> сфере автоматизированного экологического контроля.</p>
+						</div>
+					</div>
+					<div class="title">КАК УСТРОЕНА СИСТЕМА ЭКОЛОГИЧЕСКОГО МОНИТОРИНГА<br/> ПРОМЫШЛЕННЫХ ПРЕДПРИЯТИЙ НА ПРИМЕРЕ МНПЗ</div>
+				</div>
+
+
+				<div class="eco-monitoring">
+					<iframe width="100%" height="315" src="https://www.youtube.com/embed/qJ0BzRQU1Z8?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+					<?php /*<img src="/images/eco-monitoring.jpg" alt="">*/?>
+				</div>
+
+				<div class="container">
+					<div class="block block-3">
+						<div class="block__content">
+							<p>Датчики системы смонтированы на восьми ключевых установках предприятия,<br/> определенных «Мосэкомонитрингом».</p>
+							<p>Кроме того, предприятие представляет информацию о состоянии воздуха на<br/> заводе и в его окрестностях. Все заинтересованные могут получить ее в<br/> режиме реального времени на сайте и на специальном «Экоинформере»,<br/> установленном на улице Капотня.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="screen-6">
+				<div class="container">
+					<div class="block block-4">
+						<div class="block__title">
+							КАК МОСКОВСКИЕ ПРЕДПРИЯТИЯ СНИЖАЮТ ВОЗДЕЙСТВИЕ<br/>
+							НА ОКРУЖАЮЩУЮ СРЕДУ
+						</div>
+						<div class="block__content">
+							<p>Кроме комплексного мониторинга выбросов, в Москве ведется и работа по их<br/> снижению. Уменьшить воздействие транспорта удалось благодаря переходу на<br/> топливо Евро-5, организации транспортных потоков и озеленению.</p>
+							<p>Одновременно идет модернизации промышленности, внедряются так<br/> называемые наилучшие природоохранные технологии. ТЭЦ переходят на<br/> экологичное газовое топливо, модернизируются Курьяновские и Люберецкие<br/> очистные сооружения «Мосводоканала». Так, на Курьяновских очистных все<br/> емкости полностью закрыты специальными перекрытиями и воздушными<br/> фильтрами. На Люберецких устанавливают современное газоочистительное<br/> оборудование и   меняют морально устаревшие прессы на современные<br/> центрифуги. Эти работы планируется завершить к 2020 году.</p> 
+							<p>С 2011 года модернизацию Московского НПЗ ведет и компания «Газпром<br/> нефть». Благодаря этому предприятие к 2015 году на 50% снизило общее<br/> воздействие на окружающую среду, а к 2020 году уменьшит его еще вдвое.</p>
+							<p>В Год экологии «Газпром нефть» завершила строительство на заводе<br/> инновационных очистных сооружений «Биосфера». Новый комплекс увеличит<br/> эффективность очистки сточных вод завода до 99.9%. Предприятие перейдет<br/> почти на замкнутый цикл водопотребления, значительно снизив нагрузку на<br/> городские системы очистки воды. Строительство «Биосферы» распоряжением<br/> Правительства РФ было включено в федеральный план мероприятий<br/> по проведению Года экологии в России. Аналогичные очистные сооружения<br/>s компания строит и на Омском НПЗ.</p> 
+						</div>
+					</div>
+				</div>
+				<div class="treatment-facilities">
+					<iframe width="100%" height="315" src="https://www.youtube.com/embed/tp30FLzdaCs?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+					<?php /*<img src="/images/treatment-facilities.jpg" alt="">*/?>
+				</div>
+			</div>
+
+			<div id="screen-7">
+				<div class="container">
+					<div class="image"><img src="/images/sobyanin.png" alt=""></div>
+					<blockquote>«Завершен очередной этап модернизации (Московского НПЗ – ред.). Построены<br/> очистные сооружения „Биосфера“, которые обеспечат максимально полное<br/> очищение сточных вод. Кроме того, предприятие в 2,5 раза сократит забор воды<br/> из Москвы-реки за счет повторного использования очищенных вод в рамках<br/> замкнутого цикла. Это хорошая новость для всех жителей столицы<br/> и Подмосковья. В особенности для тех, кто живет рядом с заводом и ходит<br/> гулять в парки, расположенные в нижнем течении Москвы-реки»</blockquote>
+					<div class="position">Мэр Москвы</div>
+					<div class="name">Сергей Собянин</div>
+				</div>
+			</div>
+			
+			<div id="screen-8">
+				<div class="container">
+					<div class="block block-5">
+						<div class="block__title">
+							ЭКОЛОГИЧЕСКАЯ СТРАТЕГИЯ МОСКВЫ ДО 2030 ГОДА
+						</div>
+						<div class="block__content">
+							<p>Благодаря мерам по снижению выбросов автомобилей, модернизации<br/> городского хозяйства и промышленности, Москва к 2030 году ставит цель по<br/> значительному улучшению экологических параметров.</p>
+							<p>По словам руководителя столичного департамента природопользования и<br/> охраны окружающей среды (ДПОСС) Антона Кульбачевского, уменьшение<br/> выбросов в атмосферу - одна из главных задач, стоящих перед столичным<br/> руководством. Ведь когда очищается воздух, становятся чище и все остальные<br/> среды: и вода, и почва.</p>
+							<p>«Наиболее важными я считаю положения по улучшению качества воздуха на 30<br/> процентов возле автомобильных трасс и на 20 процентов — в районе жилой<br/> застройки к 2030 году», - комментирует Кульбачевский проект «Экологической<br/> стратегии Москвы до 2030 года», главного городского документа в области<br/> охраны окружающей среды.</p>
+							<p>Также в планах столичных властей снижение выбросов веществ с неприятным<br/> запахом на 70% от уровня 2013 года и уменьшение взвешенных частиц на 20%<br/> от уровня 2016 года.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="strategy-2030">
+
+					<div class="big-title">Целевые показатели &laquo;Экологической стратегии Москвы до 2030 года&raquo;</div>
+					
+					<div class="ab-wrapper">
+
+						<div class="ab ab-1">
+							
+							<div class="ab-title">Снижение уровня загрязнения</div>
+							
+							<div class="a-cols">
+								<div class="a-col a-col-1">
+
+									<div class="anime-block">
+										<div class="pie_progress" data-goal="30" role="progressbar">
+											<div class="pie_progress__number">0%</div>
+										</div>
+									</div>
+
+									<div class="label-block">атмосферного воздуха<br/> на территориях вблизи автотрасс</div>
+
+								</div>
+
+								<div class="a-col a-col-2">
+
+									<div class="anime-block">
+										<div class="pie_progress" data-goal="20" role="progressbar">
+											<div class="pie_progress__number">0%</div>
+										</div>
+									</div>
+
+									<div class="label-block">атмосферного воздуха<br/> в жилых территориях города</div>
+									
+								</div>	
+							</div>
+
+
+						</div>
+						
+						<div class="ab-2-wrap">
+							<div class="ab ab-2">
+								
+								<div class="ab-title">Снижение выбросов</div>
+
+								<div class="a-col a-col-3">
+
+									<div class="anime-block">
+										<div class="pie_progress" data-goal="70" role="progressbar">
+											<div class="pie_progress__number">0%</div>
+										</div>
+									</div>
+
+									<div class="label-block">от объектов очистки сточных вод<br/> хозяйственно-бытовой канализации<br/> с неприятных запахом</div>
+									
+								</div>
+
+							</div>
+
+							<div class="ab ab-3">
+								
+								<div class="ab-title">Снижение поступления</div>
+
+								<div class="a-col a-col-4">
+
+									<div class="anime-block">
+										<div class="pie_progress" data-goal="20" role="progressbar">
+											<div class="pie_progress__number">0%</div>
+										</div>
+									</div>
+
+									<div class="label-block">взмешенных частиц<br/> в атмосферный воздух</div>
+									
+								</div>
+
+							</div>
+						</div>
+
+					</div>
+
+					<div class="after-anime">
+
+						<div class="aa aa-1">2 623,95 тыс. тонн</div>
+
+						<div class="aa aa-2">
+							<div class="aa2-title">Снижение выбросов</div>
+							<div class="aa2-body">парниковых газов к 2018 году эквивалента СО2</div>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div><!-- end all-wrapper -->
+
+	</div><!-- end page-wrapper -->
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,88 @@
+(function($){
+	$(window).ready(function(){
+		$('iframe').each(function(){
+			$(this).height($(this).width()/16*9);
+		});
+	});
+
+	$(window).load(function(){
+		$('iframe').each(function(){
+			$(this).height($(this).width()/16*9);
+		});
+		$(window).resize(function(){
+			$(this).height($(this).width()/16*9);
+		});
+
+		$('.pie_progress').asPieProgress({
+		    'namespace': 'pie_progress',
+		    'speed': 55,
+		    'barsize': '21',
+		    'barcolor': '#98D832',
+		    'trackcolor': 'transparent'
+
+		});
+
+		if( $(window).height() < $('#screen-1 img.bg').height()) {
+			$('#screen-1').addClass('to_big');
+		} else {
+			$('#screen-1').removeClass('to_big');
+		}
+
+		/*$("iframe").waypoint(function(direction) {
+		  $(this).attr('src',$(this).attr('src')+'&autoplay=1');
+		});*/
+
+		$(".pie_progress").waypoint(function(direction) {
+		  $(this).asPieProgress('start');
+		},{
+		    offset: 'bottom-in-view',
+		    triggerOnce: true 
+		});
+		Share = {
+			vkontakte: function(purl, ptitle, pimg, text) {
+				url  = 'http://vkontakte.ru/share.php?';
+				url += 'url='          + encodeURIComponent(purl);
+				url += '&title='       + encodeURIComponent(ptitle);
+				url += '&description=' + encodeURIComponent(text);
+				url += '&image='       + encodeURIComponent(pimg);
+				url += '&noparse=true';
+				Share.popup(url);
+			},
+			odnoklassniki: function(purl, text) {
+				url  = 'http://www.ok.ru/dk?st.cmd=addShare&st.s=1';
+				url += '&st.comments=' + encodeURIComponent(text);
+				url += '&st._surl='    + encodeURIComponent(purl);
+				Share.popup(url);
+			},
+			facebook: function(purl, ptitle, pimg, text) {
+				url  = 'http://www.facebook.com/sharer.php?s=100';
+				url += '&p[title]='     + encodeURIComponent(ptitle);
+				url += '&p[summary]='   + encodeURIComponent(text);
+				url += '&p[url]='       + encodeURIComponent(purl);
+				url += '&p[images][0]=' + encodeURIComponent(pimg);
+				Share.popup(url);
+			},
+			twitter: function(purl, ptitle) {
+				url  = 'http://twitter.com/share?';
+				url += 'text='      + encodeURIComponent(ptitle);
+				url += '&url='      + encodeURIComponent(purl);
+				url += '&counturl=' + encodeURIComponent(purl);
+				Share.popup(url);
+			},
+			mailru: function(purl, ptitle, pimg, text) {
+				url  = 'http://connect.mail.ru/share?';
+				url += 'url='          + encodeURIComponent(purl);
+				url += '&title='       + encodeURIComponent(ptitle);
+				url += '&description=' + encodeURIComponent(text);
+				url += '&imageurl='    + encodeURIComponent(pimg);
+				Share.popup(url);
+			},
+
+			popup: function(url) {
+			    window.open(url,'','toolbar=0,status=0,width=626,height=436');
+			}
+		};
+
+	});
+
+})(jQuery);
